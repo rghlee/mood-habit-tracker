14e1a745f01d9982b10dcb5646efabb75aaa1bdf
rghlee <140765098+rghlee@users.noreply.github.com>
2026-03-29 23:52:42 +0000

    Updated

--- mood-habit-tracker/components/MoodSelector.js
+++ mood-habit-tracker/components/MoodSelector.js
@@ -11,7 +11,7 @@ export default function MoodSelector({ setMood }) {
   };
 
   return (
-    <View style={{ flexDirection: "row", justifyContent: "space-around", marginVertical: 5 }}>
+    <View style={{ flexDirection: "row", justifyContent: "space-around", margin: 5, padding:5, marginVertical: 10, flexWrap: "wrap"}}>
 
       <TouchableOpacity
         onPress={() => handleSelect("Happy")}
@@ -22,6 +22,39 @@ export default function MoodSelector({ setMood }) {
         }}
       >
         <Text>😊 Happy</Text>
+      </TouchableOpacity>
+
+    <TouchableOpacity
+        onPress={() => handleSelect("Confused")}
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          backgroundColor: selectedMood === "Confused" ? "#90CAF9" : "#E0E0E0"
+        }}
+      >
+        <Text>🤔 Confused</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => handleSelect("Loving")}
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          backgroundColor: selectedMood === "Loving" ? "#90CAF9" : "#E0E0E0"
+        }}
+      >
+        <Text> 💖 Loving</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => handleSelect("Annoyed")}
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          backgroundColor: selectedMood === "Annoyed" ? "#90CAF9" : "#E0E0E0"
+        }}
+      >
+        <Text> 😒 Annoyed</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
@@ -36,6 +69,17 @@ export default function MoodSelector({ setMood }) {
       </TouchableOpacity>
 
       <TouchableOpacity
+        onPress={() => handleSelect("Embarrased")}
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          backgroundColor: selectedMood === "Embarrassed" ? "#90CAF9" : "#E0E0E0"
+        }}
+      >
+        <Text>😳 Embarrassed</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
         onPress={() => handleSelect("Sad")}
         style={{
           padding: 10,
@@ -44,6 +88,61 @@ export default function MoodSelector({ setMood }) {
         }}
       >
         <Text>😞 Sad</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => handleSelect("Confident")}
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          backgroundColor: selectedMood === "Confident" ? "#90CAF9" : "#E0E0E0"
+        }}
+      >
+        <Text>😎 Confident</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => handleSelect("Overwhelmed")}
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          backgroundColor: selectedMood === "Overwhelmed" ? "#90CAF9" : "#E0E0E0"
+        }}
+      >
+        <Text>😰 Overwhelmed</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity
+        onPress={() => handleSelect("Frustrated")}
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          backgroundColor: selectedMood === "Frustrated" ? "#90CAF9" : "#E0E0E0"
+        }}
+      >
+        <Text>😤 Frustrated</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => handleSelect("Unwell")}
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          backgroundColor: selectedMood === "Unwell" ? "#90CAF9" : "#E0E0E0"
+        }}
+      >
+        <Text> 🤢Unwell</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => handleSelect("Heated")}
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          backgroundColor: selectedMood === "Heated" ? "#90CAF9" : "#E0E0E0"
+        }}
+      >
+        <Text> 😡 Heated</Text>
       </TouchableOpacity>
 
     </View>
